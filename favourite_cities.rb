e8127138d94a9ffb2df36a82af4155347bb83d69
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-19 12:06:33 -0400

    Project Uploads

---
+++ favourite_cities.rb
@@ -0,0 +1,3 @@
+favourite_cities = ["Toronto", "Amsterdam", "Madrid", "Paris", "Anaheim"]
+	puts "My five favourite cities are:"
+	favourite_cities.each { |x| puts x}
---
+++ quasicalc.rb
@@ -0,0 +1,6 @@
+def add(a,b)
+	sum = a+b
+	puts "#{a} plus #{b} equals #{sum}"
+end
+
+add(93, 14)
