42b8a2adc5ab4f3f206904c616b3b7895f82b3ef
JON HOUSE <J_HOUSE974@YAHOO.CA>
2014-10-19 12:03:58 -0400

    Message

---
+++ BM_Labs_Array.rb
@@ -0,0 +1,13 @@
+(1..100).each do |x|
+	case
+	when x % 3 == 0 && x % 5 == 0
+		puts "Bitmaker Labs"
+	when x % 3 == 0
+		puts "Bitmaker"
+	when % 5 == 0
+		puts "Labs"
+	else
+		puts x
+end
+end
+		
